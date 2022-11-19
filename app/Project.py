6394caab5db1ebc10395bda25d8760367085b040
Robbe Sas <91063303+SasRobbe@users.noreply.github.com>
2022-11-19 15:10:19 +0100

    updated api

--- app/Project.py
+++ app/Project.py
@@ -5,6 +5,7 @@ import random
 
 app = FastAPI()
 
+# Implementing security with CORS
 origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
@@ -23,6 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# List with data (includes song-names, artists and playlists)
 spotify_list = [
     {
         "songName": "Cry Baby",
@@ -143,10 +145,51 @@ spotify_list = [
         "songName": "Dizzy",
         "artist": "MISSIO",
         "playlist": "Ye arc"
+    },
+    {
+        "songName": "Feeling Like The End",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "Die For You",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "Before The Day Is Over",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "Dissolve",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "NIGHT RIDER",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "Glimpse of Us",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "BLAHBLAHBLAH DEMO",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
+    },
+    {
+        "songName": "1AM FREESTYLE",
+        "artist": "Joji",
+        "playlist": "Bus Vibes"
     }
 ]
 
 
+# define the classes
 class spotify_out(BaseModel):
     songName: str
     artist: str
@@ -159,18 +202,21 @@ class spotify_in(BaseModel):
     playlist: str | None = Field(min_length=3)
 
 
+# gets a random song with their corresponding artist and playlist from the data defined above
 @app.get("/spotify", response_model=spotify_out)
 async def return_spotify():
     random_int = random.randint(0, len(spotify_list)-1)
     return spotify_list[random_int]
 
 
+# add a new song with their corresponding artist and playlist
 @app.post("/spotify", response_model=list[spotify_out])
 async def insert_spotify(spotify: spotify_in):
     spotify_list.append(spotify.dict())
     return spotify_list
 
 
+# gets all songs of a specific playlist from the data defined above
 @app.get("/playlists")
 async def return_playlist(
     playlist: str = Query(
@@ -190,3 +236,15 @@ async def return_playlist(
     return {"playlists": songs}
 
 
+# gets all songs of a specific artist from the data defined above
+@app.get("/artist/{getArtist}")
+async def return_songs_of_artist(get_artist: str):
+    songs2 = []
+    for y in range(len(spotify_list)):
+        if get_artist == spotify_list[y]['artist']:
+            artist_songs = {}
+            artist_songs["songName"] = spotify_list[y]['songName']
+            artist_songs["artist"] = spotify_list[y]['artist']
+            artist_songs["playlist"] = spotify_list[y]['playlist']
+            songs2.append(artist_songs)
+    return {"artists": songs2}
